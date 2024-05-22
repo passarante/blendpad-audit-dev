4f9d7ddc8f8e3bb120b4559145c7334b72a02b7e
Guney Dirim <passarante@gmail.com>
2024-05-23 02:17:36 +0300

    deleted getcount route

--- app/api/getCounts/route.ts
+++
@@ -1,9 +0,0 @@
-import { getContractCount } from "@/actions/scrape-contract";
-
-import { NextApiRequest } from "next";
-
-export async function GET(req: NextApiRequest) {
-    const response = await getContractCount();
-
-    return Response.json({ response });
-}
